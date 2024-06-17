89db7bb40177598fe23747262a740900b965d79e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-06-17 04:18:31 -0400

    Feat: forward and backward shuffling

--- crate_mod_shuffle/src/main.rs
+++ crate_mod_shuffle/src/main.rs
@@ -2,21 +2,41 @@
 
 use derive_builder::Builder;
 use mod_shuffle::{error::Result,
-                  shuffle::{ModShuffler, ModShufflerBuilder}};
+                  shuffle::{ProposedModShuffler, ProposedModShufflerBuilder}};
 
 fn main() -> Result<()> {
-    let new_made = ModShuffler::new(1, 2, 3, 3);
+    let new_made = ProposedModShuffler::new(1, 2, 3, 3);
+    let new_made = new_made.validate()?;
     println!("ModShuff w/ NEW: {:?}", new_made);
 
-    let def_made = ModShuffler::default();
+    let def_made = ProposedModShuffler::default();
+    let def_made = def_made.validate()?;
     println!("ModShuff w/ DEFAULT: {:?}", def_made);
 
-    let build_made = ModShufflerBuilder::default().val(11)
-                                                  .modulo(22)
-                                                  .prime_forward_shuffler(7)
-                                                  .prime_backward_shuffler(19)
-                                                  .build()?;
+    let build_made = ProposedModShufflerBuilder::default().val(10)
+                                                          .modulo(22)
+                                                          .prime_forward_shuffler(7)
+                                                          .prime_backward_shuffler(19)
+                                                          .build()?;
+    let build_made = build_made.validate()?;
     println!("ModShuff w/ BUILD: {:?}", build_made);
+    println!("-------------------------------------------");
+
+    let new2 = new_made.fwd_shuffle();
+    let def2 = def_made.fwd_shuffle();
+    let build2 = build_made.fwd_shuffle();
+    println!("new2 {:?}", new2);
+    println!("def2 {:?}", def2);
+    println!("build2 {:?}", build2);
+    println!("-------------------------------------------");
+
+    let new3 = new2.rev_shuffle();
+    let def3 = def2.rev_shuffle();
+    let build3 = build2.rev_shuffle();
+    println!("new3 {:?}", new3);
+    println!("def3 {:?}", def3);
+    println!("build3 {:?}", build3);
+    println!("-------------------------------------------");
 
     Ok(())
 }
--- crate_mod_shuffle/src/shuffle.rs
+++ crate_mod_shuffle/src/shuffle.rs
@@ -3,10 +3,9 @@
 use derive_builder::Builder;
 use derive_more::Constructor;
 
-/// Silly struct. Value, what it's modulo, and a forward and reverse (inverse) prime for shuffling
-///
-#[derive(Clone, Debug, Builder, Constructor)]
-// Note: Buidler takes `structnameBUILDER` as struct start, *not* `structname`
+use crate::error::Error;
+
+#[derive(Debug, Clone)]
 pub struct ModShuffler {
     val:                     u64,
     modulo:                  u64,
@@ -14,12 +13,60 @@ pub struct ModShuffler {
     prime_backward_shuffler: u64,
 }
 
-impl Default for ModShuffler {
-    fn default() -> Self {
-        ModShuffler { val:                     0,
-                      modulo:                  32,
-                      prime_forward_shuffler:  3,
-                      prime_backward_shuffler: 11, }
+/// Generated via validation of ProposedModShuffler (I should really wrap into the build method)
+impl ModShuffler {
+    pub fn fwd_shuffle(&self) -> Self {
+        // TODO: rel cost of 128bit cating and action, vs testing and contitionally running
+        let mut v = self.val as u128;
+        let p = self.prime_forward_shuffler as u128;
+        let m = self.modulo as u128;
+
+        let v = ((v * p) % m) as u64;
+
+        let mut out = self.clone();
+        out.val = v;
+        out
+    }
+
+    pub fn rev_shuffle(&self) -> Self {
+        // TODO: rel cost of 128bit cating and action, vs testing and contitionally running
+        let mut v = self.val as u128;
+        let r = self.prime_backward_shuffler as u128;
+        let m = self.modulo as u128;
+
+        let v = ((v * r) % m) as u64;
+
+        let mut out = self.clone();
+        out.val = v;
+        out
     }
 }
-impl ModShuffler {}
+
+/// Silly struct. Value, what it's modulo, and a forward and reverse (inverse) prime for shuffling
+///
+#[derive(Clone, Debug, Builder, Constructor)]
+// Note: Buidler takes `structnameBUILDER` as struct start, *not* `structname`
+pub struct ProposedModShuffler {
+    val:                     u64,
+    modulo:                  u64,
+    prime_forward_shuffler:  u64,
+    prime_backward_shuffler: u64,
+}
+
+impl Default for ProposedModShuffler {
+    fn default() -> Self {
+        ProposedModShuffler { val:                     0,
+                              modulo:                  32,
+                              prime_forward_shuffler:  3,
+                              prime_backward_shuffler: 11, }
+    }
+}
+
+impl ProposedModShuffler {
+    pub fn validate(&self) -> Result<ModShuffler, Error> {
+        Ok(ModShuffler { val:                     self.val,
+                         modulo:                  self.modulo,
+                         prime_forward_shuffler:  self.prime_forward_shuffler,
+                         prime_backward_shuffler: self.prime_backward_shuffler, })
+    }
+}
